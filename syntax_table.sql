33592a5db5f7ef574c5ec94d28b4940cb1e56dbb
rusov <rusov63@yahoo.com>
2024-02-15 15:43:04 +0400

    one commit

---
+++ syntax_table.sql
@@ -0,0 +1,57 @@
+select *
+from list_medications
+
+-- Сортировка по возростанию
+select * 
+from list_medications
+order by name
+-- или по убыванию
+select * 
+from list_medications
+order by name desc
+
+-- Сортировка с помощью оператора max, min 
+select max(list_id), min(list_id)
+from list_medications
+
+-- Фильтрация строк с помощью where. Вывести пустую колонку
+select manufacturer
+from list_medications
+where manufacturer is null
+
+
+-- вывести все Сердечно Сосудистые или Жаропонижающие
+select *
+from list_medications
+where category = 'Сердечно Сосудистые' or category = 'Жаропонижающие'
+
+-- находит значения  которые начинаются на "Н" первые две записи  по убыванию
+select *
+from list_medications
+where active_substance like 'Н%'
+order by name desc
+limit 2
+
+-- возвращает только те строки которые присутствуют в обох запросах
+select category
+from list_medications
+intersect
+select category
+from category
+
+
+-- Удаление таблицы
+drop table list_medications;
+-- удаление столбца в таблице list_medications
+alter table list_medications drop column release_form;
+
+-- Переименовать таблицу
+alter table active_substance rename to activeSubstance;
+-- переименовать столбец
+alter table active_substance rename active_id to number_id;
+
+-- Добавление нового столбца
+alter table category add column description text;
+
+-- Изменение типа данных столбца
+alter table list_medications alter column name set data type varchar(50);
